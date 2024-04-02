625ab041482bbaaf6e6a74d1eb6d7638b71c60f2
Juan David <juangiraldopb@gmail.com>
2024-04-02 13:12:06 -0500

    Creacion del constructor

--- src/com/alejandro/taller/punto2/dominio/Snack.java
+++ src/com/alejandro/taller/punto2/dominio/Snack.java
@@ -1,4 +1,15 @@
 package com.alejandro.taller.punto2.dominio;
 
 public class Snack {
+    private String nombre;
+    private String codigo;
+    private int precio;
+    private int cantidad;
+
+    public Snack(String nombre, String codigo, int precio, int cantidad){
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.precio = precio;
+        this.cantidad = cantidad;
+    }
 }
