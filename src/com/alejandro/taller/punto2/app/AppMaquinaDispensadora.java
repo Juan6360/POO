3f93f8ef02665a87326fc7c3a84c001d327a2dfa
Juan David <juangiraldopb@gmail.com>
2024-04-02 13:10:16 -0500

    Primera Maquina

--- src/com/alejandro/taller/punto2/app/AppMaquinaDispensadora.java
+++ src/com/alejandro/taller/punto2/app/AppMaquinaDispensadora.java
@@ -1,8 +1,12 @@
 package com.alejandro.taller.punto2.app;
 
+import com.alejandro.taller.punto2.dominio.MaquinaDispensadora;
+
 public class AppMaquinaDispensadora {
 
     public static void main(String[] args){
+        MaquinaDispensadora maquinaDispensadora = new MaquinaDispensadora();
 
+        maquinaDispensadora.agregarUnSnack("Hershey's", "A001", 1200, 6);
     }
 }
