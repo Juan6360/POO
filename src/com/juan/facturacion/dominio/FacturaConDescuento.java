c9f988eab4738020a7fef4e726aa54197c010d22
Juan Giraldo <juangiraldopb@gmail.com>
2024-05-03 18:53:04 -0500

    Ok

--- src/com/juan/facturacion/dominio/FacturaConDescuento.java
+++ src/com/juan/facturacion/dominio/FacturaConDescuento.java
@@ -3,7 +3,12 @@ package com.juan.facturacion.dominio;
 import java.time.LocalDate;
 
 public class FacturaConDescuento extends FacturaSinIva{
+
+    private double porcDescuento;
+
     protected FacturaConDescuento(long valor, String cliente, LocalDate fecha) {
         super(valor, cliente, fecha);
     }
+
+    public long calcularDescuento(){return 0;}
 }
--- src/com/juan/facturacion/dominio/FacturaVencida.java
+++ src/com/juan/facturacion/dominio/FacturaVencida.java
@@ -3,9 +3,16 @@ package com.juan.facturacion.dominio;
 import java.time.LocalDate;
 
 public class FacturaVencida extends Factura{
+
+    public final int VLOR_DIA_VENCIDO = 10000;
+
     protected FacturaVencida(long valor, String cliente, LocalDate fecha) {
         super(valor, cliente, fecha);
     }
+
+    public int getDiasVencidos(){return 0;}
+
+    public long calcularVencimiento(){return 0;}
 
     @Override
     protected long calcularTotal() {
