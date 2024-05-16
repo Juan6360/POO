522623553006f0b3fc3b6dc0466738313789ba99
Juan Giraldo <juangiraldopb@gmail.com>
2024-05-15 23:04:28 -0500

    App/Video

--- src/com/juan/antivirus/app/AppAntivirus.java
+++ src/com/juan/antivirus/app/AppAntivirus.java
@@ -12,9 +12,12 @@ public class AppAntivirus {
         DocumentoWord word1 = new DocumentoWord("hojadevida.docx");
         DocumentoWord word2 = new DocumentoWord("hojadevida☠️.docx");
         DocumentoPDF pdf1 = new DocumentoPDF("hojadevida.pdf");
-        Video video = new Video("miprimeracomunion.mp4", 320);
+        App app1 = new Android("Meta", 10);
+        App app2 = new Ios("Gmail", 16.5);
+        Video video1 = new Mp4("miprimeracomunion.mp4", 320);
+        Video video2 = new VLC("segundaboda.mp3", 320);
 
-        List<Viruseable> elementos = Arrays.asList(word1, word2, pdf1, video);
+        List<Viruseable> elementos = Arrays.asList(word1, word2, pdf1, app1, app2, video1, video2);
         avast.escanear(elementos);
     }
 }
