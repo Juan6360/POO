1552313303b7060b527982e7a3228aecb3652770
Juan David <juangiraldopb@gmail.com>
2024-05-17 14:10:43 -0500

    Antivirus

--- src/com/juan/antivirus/app/AppAntivirus.java
+++ src/com/juan/antivirus/app/AppAntivirus.java
@@ -2,6 +2,7 @@ package com.juan.antivirus.app;
 
 import com.juan.antivirus.dominio.*;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 
@@ -16,8 +17,13 @@ public class AppAntivirus {
         App app2 = new Ios("Gmail", 16.5);
         Video video1 = new Mp4("miprimeracomunion.mp4", 320);
         Video video2 = new VLC("segundaboda.mp3", 320);
+        Ejecutable exe1 = new Windows("IntelliJIDEA", 1.19);
+        Ejecutable exe2 = new Mac("Photoshop", 18);
+        Audio audio1 = new Mp3("brunomars.mp3", LocalDate.of(2024, 5, 16));
+        Audio audio2 = new Flash("projectmanhattan.flash", LocalDate.of(2020, 2, 10));
 
-        List<Viruseable> elementos = Arrays.asList(word1, word2, pdf1, app1, app2, video1, video2);
+
+        List<Viruseable> elementos = Arrays.asList(word1, word2, pdf1, app1, app2, video1, video2, exe1, exe2, audio1, audio2);
         avast.escanear(elementos);
     }
 }
