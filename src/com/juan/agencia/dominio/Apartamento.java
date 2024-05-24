1a2d8fed187b4843406e586f7683a672e4ded465
Juan Giraldo <juangiraldopb@gmail.com>
2024-05-24 18:54:35 -0500

    Agencia de Arrendamiento

--- src/com/juan/agencia/dominio/Apartamento.java
+++ src/com/juan/agencia/dominio/Apartamento.java
@@ -1,4 +1,13 @@
 package com.juan.agencia.dominio;
 
 public class Apartamento extends Inmueble implements Arrendable{
+    @Override
+    public void arrendar() {
+
+    }
+
+    @Override
+    public void devolver() {
+
+    }
 }
--- src/com/juan/agencia/dominio/Casa.java
+++ src/com/juan/agencia/dominio/Casa.java
@@ -1,4 +1,13 @@
 package com.juan.agencia.dominio;
 
 public class Casa extends Inmueble implements Arrendable{
+    @Override
+    public void arrendar() {
+
+    }
+
+    @Override
+    public void devolver() {
+
+    }
 }
